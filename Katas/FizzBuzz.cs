999a960624bbf57bd82a3555276b0f9482ac87c1
Ivan Iglesias <iiglesiasbilbao@gmail.com>
2018-07-28 13:30:59 +0200

    Fizz Buzz usando delegados tipo Func.

--- Katas/FizzBuzz.cs
+++ Katas/FizzBuzz.cs
@@ -4,22 +4,11 @@ namespace Katas
 {
     public class FizzBuzz
     {
+
+        /**
+         * Fizz Buzz mediante condicionales.
+         */
         public static string Print(int Number)
-        {
-            string response = "";
-
-            for (int i = 1; i <= Number; i++)
-            {
-                if (IsFizz(i)) response += "Fizz";
-                if (IsBuzz(i)) response += "Buzz";
-                if (!IsFizz(i) && !IsBuzz(i)) response += $"{i}";
-                response += ", ";
-            }
-
-            return response.TrimEnd();
-        }
-
-        public static string Print(int Number, string[] Triggers)
         {
             string response = "";
 
@@ -37,5 +26,27 @@ namespace Katas
         private static bool IsFizz(int i) => i % 3 == 0;
 
         private static bool IsBuzz(int i) => i % 5 == 0;
+
+
+        /**
+         * Fizz Buzz usando delegados genericos tipo Func, los cuales se pasasan
+         * mediante argumento para poder escalarlo con nuevas condiciones.
+         */
+        public static string Print(int Number, Func<int, string> [] Triggers)
+        {
+            string response = "";
+
+            for (int i = 1; i <= Number; i++)
+            {
+                foreach (Func<int, string> Trigger in Triggers)
+                {
+                    response += Trigger(i);
+                }
+                response += ", ";
+            }
+
+            return response.TrimEnd();
+        }
+
     }
 }
--- Katas/Program.cs
+++ Katas/Program.cs
@@ -6,7 +6,16 @@ namespace Katas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(FizzBuzz.Print(35));
+            Console.WriteLine(FizzBuzz.Print(10));
+
+
+            Func<int, string>[] triggers = {
+                (int i) => { return i % 3 == 0 ? "Fizz" : ""; },
+                (int i) => { return i % 5 == 0 ? "Buzz" : ""; },
+                (int i) => { return i % 3 != 0 && i % 5 != 0 ? $"{i}" : ""; }
+            };
+
+            Console.WriteLine(FizzBuzz.Print(10, triggers));
         }
     }
 }
